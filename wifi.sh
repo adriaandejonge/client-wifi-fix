e2024917cee57d120528813d58064fafb6dd570e
Adriaan de Jonge <adriaandejonge@Adriaans-MacBook-Pro-2.local>
2014-04-30 14:18:56 +0200

    Refactoring: add function

--- wifi.sh
+++ wifi.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+function acceptConditions {
+		curl -X POST --data "buttonClicked=4&redirect_url=&err_flag=0" -k https://1.1.1.1/login.html	
+}
+
 networksetup -setairportpower en0 off
 networksetup -setairportpower en0 on
-curl -X POST --data "buttonClicked=4&redirect_url=&err_flag=0" -k https://1.1.1.1/login.html
+acceptConditions
+
 if [ $? -ne 0 ]; then
         sleep 30
-        curl -X POST --data "buttonClicked=4&redirect_url=&err_flag=0" -k https://1.1.1.1/login.html
+        acceptConditions
 fi
-
-
-
-
