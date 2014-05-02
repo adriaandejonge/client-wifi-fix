3a2f5bf3f7774e1250284eb39a9d4a54e35ab4a3
Adriaan de Jonge <adriaandejonge@Adriaans-MacBook-Pro-2.local>
2014-05-02 08:57:54 +0200

    Create loop

--- wifi.sh
+++ wifi.sh
@@ -4,11 +4,21 @@ function acceptConditions {
 		curl -X POST --data "buttonClicked=4&redirect_url=&err_flag=0" -k https://1.1.1.1/login.html	
 }
 
-networksetup -setairportpower en0 off
-networksetup -setairportpower en0 on
-acceptConditions
+function doIteration {
+	networksetup -setairportpower en0 off
+	networksetup -setairportpower en0 on
+	acceptConditions
 
-if [ $? -ne 0 ]; then
-        sleep 30
-        acceptConditions
-fi
+	if [ $? -ne 0 ]; then
+	        sleep 30
+	        acceptConditions
+	fi
+}
+
+for i in {1..50}
+do
+   echo "Do it $i time"
+   doIteration
+   echo "Wait $i time"
+   sleep 1500
+done
